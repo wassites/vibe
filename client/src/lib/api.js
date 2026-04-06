e868e7c39c4967432bc7fde6d4f201f174478359
walterlan alves <165735799+wasgba@users.noreply.github.com>
2026-04-06 11:38:15 -0300

    fix: corrige api.js sem caracteres extras

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -1,1 +1,3 @@
-export const API_URL = window.location.hostname === "localhost" ? `http://${window.location.hostname}:3001` : "https://vibe-server-dy2z.onrender.com";
+export const API_URL = window.location.hostname === "localhost"
+  ? `http://${window.location.hostname}:3001`
+  : "https://vibe-server-dy2z.onrender.com";
